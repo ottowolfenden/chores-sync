8abcf0452b5623c81d61793dc5c296c13791512e
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-08 01:32:03 +0100

    move remaining js to ts and commit js files

---
+++ js/modules/jsonbin.js
@@ -0,0 +1,23 @@
+const binId = "6a75f311f5f4af5e29f80b3e";
+const get = async (accessor) => {
+    const accessKey = localStorage.getItem("access-key");
+    if (!accessKey)
+        return null;
+    const headers = {
+        "X-Access-Key": accessKey,
+        "X-Bin-Meta": "false"
+    };
+    if (accessor)
+        headers["X-JSON-Path"] = accessor;
+    const response = await fetch(`https://api.jsonbin.io/v3/b/${binId}/latest`, {
+        method: "GET",
+        headers: headers
+    });
+    if (!response.ok)
+        return null;
+    let text = await response.text();
+    console.log(text);
+    return text;
+};
+export { get };
+//# sourceMappingURL=jsonbin.js.map
--- js/pages/login.js
+++ js/pages/login.js
@@ -1,17 +1,16 @@
 if (localStorage.getItem("access-key") && localStorage.getItem("name"))
     location.replace("today.html");
-
 const form = document.querySelector("form");
-
 form?.addEventListener("submit", e => {
     e.preventDefault();
-    const data = new FormData(e.currentTarget as HTMLFormElement);
-    const name = data.get("name") as string;
-    const accessKey = data.get("access-key") as string;
-
-    if (!name || !accessKey) return;
-
+    const data = new FormData(e.currentTarget);
+    const name = data.get("name");
+    const accessKey = data.get("access-key");
+    if (!name || !accessKey)
+        return;
     localStorage.setItem("name", name);
     localStorage.setItem("access-key", accessKey);
     location.replace("../index.html");
 });
+export {};
+//# sourceMappingURL=login.js.map
