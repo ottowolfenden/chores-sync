5549e9461823570c0a92d2366673f8a0c39a19db
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-09 03:05:24 +0100

    write db schema

--- pages/src/types.ts
+++ pages/src/types.ts
@@ -1,22 +1,25 @@
-type Person = {
+type Member = {
+    id: number;
     name: string;
+    isActive: boolean;
     canEditToday: boolean;
     canEditHistory: boolean;
 };
 
-type Assignment = {
-    quantity: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-    person: Person;
-    completed: boolean;
-};
-
 type Chore = {
+    id: number;
     name: string;
     icon: string;
     daily: boolean;
-    turn: Person;
-    assignments: Assignment[];
-    date: Date;
 };
 
-export type { Person, Assignment, Chore };
+type Assignment = {
+    id: number;
+    chore: Chore;
+    member: Member;
+    date: Date;
+    quantity: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+    completed: boolean;
+};
+
+export type { Member, Assignment, Chore };
