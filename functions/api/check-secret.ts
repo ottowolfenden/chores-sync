1408053752a0204ea86a45f82c72d620616d0a84
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-10 03:00:11 +0100

    attempt to fix api

--- functions/api/check-secret.ts
+++ functions/api/check-secret.ts
@@ -1,10 +1,8 @@
-import { PagesFunction, Response as CFResponse } from "@cloudflare/workers-types";
+import type { PagesFunction } from "@cloudflare/workers-types";
 
-export const onRequest: PagesFunction<{ LOGIN_SECRET: string }> = async context => {
+export const onRequest: PagesFunction<{ LOGIN_SECRET: string }> = (async context => {
     const guess = context.request.headers.get("X-Login-Secret");
     if (!guess || guess != context.env.LOGIN_SECRET)
-        return CFResponse.json({ error: "Incorrect login secret" }, { status: 401 });
-    return new CFResponse(JSON.stringify({ message: "Correct login secret" }), {
-        headers: { "Content-Type": "application/json" }
-    });
-};
+        return Response.json({ error: "Incorrect login secret" }, { status: 401 });
+    return Response.json({ message: "Correct login secret" });
+}) satisfies PagesFunction<{ LOGIN_SECRET: string }>;
