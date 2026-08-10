9de88745f00082e05135fba36314db80b3ef11e1
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-10 16:27:56 +0100

    fix quotes typo and add port forwarding for local testing on mobile

--- npm-scripts/watch.sh
+++ npm-scripts/watch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+adb reverse tcp:8788 tcp:8788
 concurrently \
-    "npm run build; chokidar \"app/src/**/*\" -c \"npm run build\"" \
-    "wrangler pages dev app/dist --live-reload"
+    'npm run build; chokidar "app/src/**/*" -c "npm run build"' \
+    'wrangler pages dev app/dist --live-reload'
 
