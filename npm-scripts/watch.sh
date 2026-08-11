44ab6c08b8a584da46a410d53c23e32b2b6de095
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-11 23:20:18 +0100

    restart adb server before starting watch script

--- npm-scripts/watch.sh
+++ npm-scripts/watch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+adb kill-server
+adb start-server
 adb reverse tcp:8788 tcp:8788
 concurrently \
     'npm run build; chokidar "app/src/**/*" -c "npm run build"' \
     'wrangler pages dev app/dist --live-reload'
-
