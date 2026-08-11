fb1c4301694ba43b4f5d034f23c505baae5193bc
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-11 01:18:23 +0100

    style and write dismiss UI logic for your chores section reorganise buttons and inputs styling

--- app/src/scripts/sections/today.ts
+++ app/src/scripts/sections/today.ts
@@ -1,3 +1,23 @@
 document.querySelector("section#today")?.addEventListener("open", () => {
     console.log("today");
+
+    const checkboxes = document.querySelectorAll(
+        "#your-chores input[type='checkbox']"
+    ) as NodeListOf<HTMLInputElement>;
+
+    const dismissAllButton = document.querySelector(
+        "button#dismiss-all"
+    ) as HTMLButtonElement;
+
+    checkboxes.forEach(c =>
+        c.addEventListener("change", () => {
+            if ([...checkboxes].every(c => c.checked)) dismissAllButton.disabled = true;
+            else dismissAllButton.disabled = false;
+        })
+    );
+
+    dismissAllButton.addEventListener("click", () => {
+        checkboxes.forEach(c => ((c as HTMLInputElement).checked = true));
+        dismissAllButton.disabled = true;
+    });
 });
