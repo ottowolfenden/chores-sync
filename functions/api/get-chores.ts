94dbabb52ce7a385d2b282ee06bb1bff6c910a24
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-13 22:38:15 +0100

    order by fetched chores by id

--- functions/api/get-chores.ts
+++ functions/api/get-chores.ts
@@ -4,7 +4,7 @@ import type { PagesFunction } from "@cloudflare/workers-types";
 export const onRequest: PagesFunction<Env> = async context => {
     try {
         const sql = neon(context.env.DATABASE_URL);
-        return Response.json(await sql`SELECT * FROM chores;`);
+        return Response.json(await sql`SELECT * FROM chores ORDER BY chore_id;`);
     } catch (err) {
         console.error(err);
         return Response.json(null, { status: 500 });
