596fefaaf47c994851665ba5880de4365489daab
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-13 17:43:33 +0100

    rewrite check access function to check message and status in case not found and cloudflare just returns 200 and index.html

--- app/src/scripts/services/cloudflare.ts
+++ app/src/scripts/services/cloudflare.ts
@@ -1,17 +1,12 @@
-const checkAccess = async (
-    guess?: string,
-    checkName: boolean = true
-): Promise<boolean> => {
+const checkAccess = async (guess?: string, checkName: boolean = true): Promise<boolean> => {
     guess ??= localStorage.getItem("login-secret") ?? undefined;
-    return (
-        guess != undefined &&
-        (localStorage.getItem("name") != undefined || !checkName) &&
-        (
-            await fetch("/api/check-secret", {
-                headers: { "X-Login-Secret": guess }
-            })
-        ).status == 200
-    );
+    if (guess == undefined || (localStorage.getItem("name") == undefined && checkName))
+        return false;
+    const response = await fetch("/api/check-secret", {
+        headers: { "X-Login-Secret": guess }
+    });
+    const data: { message?: string } = await response.json();
+    return data?.message == "authenticated" && response.ok;
 };
 
 export { checkAccess };
