26d1bd4c518a0d724e1630c5b998cf8ad25cc3c2
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-14 11:29:33 +0100

    force wrangler to watch for changes in /app/dist by creating a file in /functions when a change is made in /app/dist

--- npm-scripts/build.sh
+++ npm-scripts/build.sh
@@ -2,3 +2,6 @@
 
 npx tsc -p app/tsconfig.json
 cp -r app/src/*.html app/src/assets app/src/styles app/dist
+
+touch functions/.reload
+(sleep 0.1 && rm -f functions/.reload) &
--- npm-scripts/watch.sh
+++ npm-scripts/watch.sh
@@ -3,6 +3,7 @@
 adb kill-server
 adb start-server
 adb reverse tcp:8788 tcp:8788
+npm run build
 concurrently \
-    'npm run build; chokidar "app/src/**/*" -c "npm run build"' \
+    'chokidar "app/src/**/*" -c "npm run build"' \
     'wrangler pages dev app/dist --live-reload'
