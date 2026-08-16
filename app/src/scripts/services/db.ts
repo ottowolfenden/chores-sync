ee93e2ff2b7eb1ad963a508cb905c44a7a36d699
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-16 13:57:32 +0100

    write function to convert db result to nested ui count type

--- app/src/scripts/services/db.ts
+++ app/src/scripts/services/db.ts
@@ -19,4 +19,32 @@ export class Db {
             })
         );
     };
+
+    static getCounts = async (): Promise<UiCount[] | null> => {
+        const guess = localStorage.getItem("secret");
+        if (!guess) return null;
+        const response = await fetch("/api/get-counts", {
+            headers: { "Authorization": guess }
+        });
+        if (!response.ok) return null;
+
+        const data: DbCount[] = await response.json();
+        const choreNames = [...new Set(data.map(d => d["chore_name"]))];
+        const memberNames = [...new Set(data.map(d => d["member_name"]))];
+        return choreNames.map(cn => ({
+            choreName: cn,
+            memberCounts: memberNames.map(mn => {
+                const records = data.filter(
+                    d => d["member_name"] == mn && d["chore_name"] == cn
+                );
+                const offsetCount = records?.find(r => r["is_offset"])?.total ?? 0;
+                const nonOffsetCount = records?.find(r => !r["is_offset"])?.total ?? 0;
+                return {
+                    memberName: mn,
+                    total: offsetCount + nonOffsetCount,
+                    offset: offsetCount
+                };
+            })
+        }));
+    };
 }
