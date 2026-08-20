43fd52b238ff51566205b206a2f3b82f0a8813dc
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-20 23:51:02 +0100

    run check access before side effect imports

--- app/src/pages/index.ts
+++ app/src/pages/index.ts
@@ -1,27 +1,27 @@
 import { checkAccess } from "../functions/cloudflare.js";
+
 if (!(await checkAccess())) location.replace("login.html");
-
-import "../components/assignment-list.js";
-import "../components/md-icon.js";
-import "../components/state-actions.js";
-import "../components/num-input.js";
-import "../components/section-nav.js";
-import "../components/status-message.js";
-
-import "../temp/sections/count.js";
-import "../temp/sections/history.js";
-import "../temp/sections/settings.js";
-import "../temp/sections/today.js";
+else
+    await Promise.all([
+        import("../components/assignment-list.js"),
+        import("../components/md-icon.js"),
+        import("../components/state-actions.js"),
+        import("../components/num-input.js"),
+        import("../components/section-nav.js"),
+        import("../components/status-message.js"),
+        import("../temp/sections/count.js"),
+        import("../temp/sections/history.js"),
+        import("../temp/sections/settings.js"),
+        import("../temp/sections/today.js")
+    ]);
 
 const handleRoute = async () => {
     document.querySelectorAll("section").forEach(s => {
         if (!s.hidden) s.dispatchEvent(new CustomEvent("close"));
         const isTarget = s.id == location.hash.replace("#", "");
         s.hidden = !isTarget;
-        if (isTarget) {
-            s.dispatchEvent(new CustomEvent("open"));
-            document.querySelector("main")?.scroll(0, 0);
-        }
+        if (isTarget) s.dispatchEvent(new CustomEvent("open"));
+        document.querySelector("main")?.scroll(0, 0);
     });
 };
 
