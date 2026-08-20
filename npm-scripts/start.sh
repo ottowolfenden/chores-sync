79321d01c7e8f25dde11cb712247529bc786988e
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-20 01:47:47 +0100

    add start script, remove concurrently dependency and add esbuild to allow scripts

---
+++ npm-scripts/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+adb kill-server
+adb start-server
+adb reverse tcp:8788 tcp:8788
+npm run build
+wrangler pages dev app/dist
--- npm-scripts/watch.sh
+++ npm-scripts/watch.sh
@@ -4,6 +4,8 @@ adb kill-server
 adb start-server
 adb reverse tcp:8788 tcp:8788
 npm run build
-concurrently \
-    'chokidar "app/src/**/*" -c "npm run build"' \
-    'wrangler pages dev app/dist --live-reload'
+(
+    chokidar "app/src/**/*" -c "npm run build" &
+    wrangler pages dev app/dist --live-reload &
+    wait
+)
