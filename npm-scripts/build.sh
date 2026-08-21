0e8b75474a1ad7b0ee358376c68e4d90066d3ccd
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-21 03:00:52 +0100

    add robots.txt, meta description and font-display: swap; for 100% lighthouse score

--- npm-scripts/build.sh
+++ npm-scripts/build.sh
@@ -4,7 +4,7 @@
 node --no-warnings fetch-material-symbols.js
 rm -rf app/dist
 mkdir -p app/dist
-cp -r app/src/*.html app/src/assets app/src/styles app/dist
+cp -r app/src/*.html app/src/robots.txt app/src/assets app/dist
 npx esbuild app/src/**/*.ts app/src/styles/*.css app/src/styles/**/*.css \
     --outdir=app/dist --bundle --format=esm --target=esnext --minify --loader:.woff2=file
 
