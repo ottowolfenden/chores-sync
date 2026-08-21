f3c5929a89f93047831f8d7ae9efc30c645e902d
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-21 17:41:00 +0100

    remove temporary service

--- server/functions/api/chores.ts
+++ server/functions/api/chores.ts
@@ -1,11 +1,9 @@
 import { neon } from "@neondatabase/serverless";
 import type { PagesFunction } from "@cloudflare/workers-types";
-import { test } from "../../services/test-service";
 
 export const onRequestGet: PagesFunction<Env> = async context => {
     try {
         const sql = neon(context.env.DATABASE_URL);
-        console.log(test(context.env));
         return Response.json(await sql`SELECT * FROM chores ORDER BY chore_name;`);
     } catch (err) {
         console.error(err);
