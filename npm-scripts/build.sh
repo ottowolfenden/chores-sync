15fbdef62c0f4dea533e4a56d361b6076d73c5f8
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-21 02:39:49 +0100

    minify js and css

--- npm-scripts/build.sh
+++ npm-scripts/build.sh
@@ -5,8 +5,8 @@ rm -rf app/dist
 mkdir -p app/dist
 npx esbuild app/src/**/*.ts --outdir=app/dist --bundle --format=esm --target=esnext
 cp -r app/src/*.html app/src/assets app/src/styles app/dist
-
-node --no-warnings update-icon-link.js
+npx esbuild app/src/**/*.ts app/src/styles/*.css app/src/styles/**/*.css \
+    --outdir=app/dist --bundle --format=esm --target=esnext --minify --loader:.woff2=file
 
 touch functions/.reload
 (sleep 0.1 && rm -f functions/.reload) &
