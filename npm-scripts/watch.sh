471398f1fabdc4d2e94465d02087d2769887c476
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-21 13:18:30 +0100

    prevent infinite loop due to material symbols being fetched and causing build.sh to run again

--- npm-scripts/watch.sh
+++ npm-scripts/watch.sh
@@ -5,7 +5,7 @@ adb start-server
 adb reverse tcp:8788 tcp:8788
 npm run build
 (
-    chokidar "app/src/**/*" -c "npm run build" &
+    chokidar "app/src/**/*" -i "app/src/assets/fonts/**" -c "npm run build" &
     wrangler pages dev app/dist --live-reload &
     wait
 )
