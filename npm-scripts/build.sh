de9ddf5b191193c94697ca321a9c88aa3747e993
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-21 01:32:41 +0100

    optimise font loading by only fetching required icons and not including grad

--- npm-scripts/build.sh
+++ npm-scripts/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
 rm -rf app/dist
 mkdir -p app/dist
 npx esbuild app/src/**/*.ts --outdir=app/dist --bundle --format=esm --target=esnext
 cp -r app/src/*.html app/src/assets app/src/styles app/dist
+
+node --no-warnings update-icon-link.js
 
 touch functions/.reload
 (sleep 0.1 && rm -f functions/.reload) &
