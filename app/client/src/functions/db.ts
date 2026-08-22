7214fc4576df36885885e3392e0fe952897d46a0
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-22 21:38:52 +0100

    complete function to get assignments by including turn member

--- app/client/src/functions/db.ts
+++ app/client/src/functions/db.ts
@@ -1,5 +1,4 @@
 import { Context } from "../classes/context.js";
-import { DateOnly } from "../classes/date-only.js";
 
 const timeout = 10000;
 
@@ -114,13 +113,11 @@ export const getCurrentMember = async (): Promise<UiMember | null> => {
     };
 };
 
-export const getTodayAssignments = async (): Promise<
-    Omit<UiAssignment, "turnMember">[] | null
-> => {
+export const getTodayAssignments = async (): Promise<UiAssignment[] | null> => {
     const guess = localStorage.getItem("secret");
     if (!guess) return null;
-    const today = new DateOnly().toString();
-    const response = await fetch(`/api/assignments?min-date=${today}`, {
+    const today = new Date().toISOString().split("T")[0];
+    const response = await fetch(`/api/assignments?date=${today}`, {
         method: "GET",
         headers: { "Authorization": guess },
         signal: AbortSignal.timeout(timeout)
@@ -130,23 +127,28 @@ export const getTodayAssignments = async (): Promise<
     const data: DbAssignment[] = await response.json();
     const chores = await Context.chores;
     const members = await Context.members;
+    const turns = await Context.turns;
 
     if (
         !chores ||
         !members ||
+        !turns ||
         !data.every(
             d =>
                 chores.some(c => c.id == d["chore_id"]) &&
-                members.some(m => m.id == d["member_id"])
+                members.some(m => m.id == d["member_id"]) &&
+                turns.some(t => t.chore.id == d["chore_id"])
         )
     )
         return null;
+
     return data.map(
-        (d): Omit<UiAssignment, "turnMember"> => ({
+        (d): UiAssignment => ({
             id: d["assignment_id"],
-            date: new DateOnly(d["assign_date"]),
+            date: d["assign_date"],
             quantity: d["quantity"],
             chore: chores.find(c => c.id == d["chore_id"])!,
+            turnMember: turns.find(t => t.chore.id == d["chore_id"])!.member,
             chosenMember: members.find(m => m.id == d["member_id"])!
         })
     );
