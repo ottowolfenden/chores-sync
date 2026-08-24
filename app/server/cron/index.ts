fe0bfeba6d34b995f0acb64c79b06f4b1c306e77
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-24 21:24:27 +0100

    write auto assign function to assign daily chores at 1am every day

--- app/server/cron/index.ts
+++ app/server/cron/index.ts
@@ -1,9 +1,29 @@
-import { getMembers } from "../services/members";
+import { getAllChores } from "../services/chores";
+import { getTurns } from "../services/turns";
+import { replaceAssignments } from "../services/assignments";
 
 const autoAssign = async (env: Env) => {
-    const result = await getMembers(env);
-    const data = result.ok ? result.data : null;
-    console.log(data);
+    const turnsResult = await getTurns(env);
+    const choresResult = await getAllChores(env);
+    const turns = turnsResult.ok ? turnsResult.data : null;
+    const chores = choresResult.ok ? choresResult.data : null;
+
+    if (!turns || !chores || !chores.every(c => turns.some(t => t.chore_id == c.chore_id)))
+        return;
+
+    const today = new Date().toISOString().split("T")[0];
+    const assignments: DbAssignment[] = chores
+        .filter(c => c.is_daily)
+        .map(c => ({
+            assignment_uuid: crypto.randomUUID(),
+            assign_date: new Date(),
+            quantity: 1,
+            is_offset: false,
+            chore_id: c.chore_id,
+            member_id: turns.find(t => t.chore_id == c.chore_id)!.member_id
+        }));
+
+    await replaceAssignments(env, assignments, today);
 };
 
 export default {
