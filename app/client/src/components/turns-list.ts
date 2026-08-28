633857f4bf81f3bdf1b45b297474a48ae0eb5458
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-28 16:18:01 +0100

    add dropdown to turns list to speed up adding someone when it is not their turn

--- app/client/src/components/turns-list.ts
+++ app/client/src/components/turns-list.ts
@@ -1,13 +1,43 @@
 import { LitElement, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 import { repeat } from "lit/directives/repeat.js";
 import { addAssignment } from "../functions/db";
+import { Context } from "../classes/context";
 
 @customElement("turns-list")
 export class TurnsList extends LitElement {
     protected createRenderRoot = () => this;
 
     @property({ type: Array }) turns: UiTurn[] = [];
+    @state() private members: UiMember[] = [];
+
+    async connectedCallback() {
+        super.connectedCallback();
+        this.members = (await Context.members) ?? [];
+    }
+
+    private addAssignment = async (
+        turn: UiTurn,
+        chosenMember: UiMember = turn.member
+    ): Promise<boolean> => {
+        const assignment = {
+            uuid: crypto.randomUUID(),
+            date: new Date(),
+            quantity: 1,
+            chore: turn.chore,
+            turnMember: turn.member,
+            chosenMember: chosenMember
+        };
+        const success = await addAssignment(assignment);
+        if (success)
+            window.dispatchEvent(
+                new CustomEvent("assignment-added", {
+                    detail: { assignment }
+                })
+            );
+        this.requestUpdate();
+        return success;
+    };
 
     render = () =>
         repeat(
@@ -21,25 +51,7 @@ export class TurnsList extends LitElement {
                         .conf=${{
                             normal: {
                                 icon: "add",
-                                click: async () => {
-                                    const assignment = {
-                                        uuid: crypto.randomUUID(),
-                                        date: new Date(),
-                                        quantity: 1,
-                                        chore: t.chore,
-                                        turnMember: t.member,
-                                        chosenMember: t.member
-                                    };
-                                    const success = await addAssignment(assignment);
-                                    if (success)
-                                        window.dispatchEvent(
-                                            new CustomEvent("assignment-added", {
-                                                detail: { assignment }
-                                            })
-                                        );
-                                    this.requestUpdate();
-                                    return success;
-                                }
+                                click: async () => await this.addAssignment(t)
                             },
                             loading: {},
                             success: {},
@@ -47,6 +59,36 @@ export class TurnsList extends LitElement {
                         }}
                         state-button-class="transparent small">
                     </state-actions>
+                    <button
+                        class="transparent small"
+                        popovertarget="turn-popover-${t.chore.id}${t.member.id}"
+                        style="anchor-name: --turn-anchor-${t.chore.id}${t.member.id}">
+                        <md-icon>arrow_drop_down</md-icon>
+                    </button>
+                    <div class="dropdown">
+                        <div
+                            popover
+                            class="dropdown"
+                            id="turn-popover-${t.chore.id}${t.member.id}"
+                            style="position-anchor: --turn-anchor-${t.chore.id}${t.member.id}">
+                            ${this.members.map(
+                                m => html`
+                                    <button
+                                        class="tonal"
+                                        @click=${async (e: Event) => {
+                                            (e.target as HTMLElement)
+                                                .closest<HTMLElement>("[popover]")
+                                                ?.hidePopover();
+                                            await this.addAssignment(t, m);
+                                            this.requestUpdate();
+                                        }}>
+                                        <md-icon>add</md-icon>
+                                        <span class="member-name">${m.name}</span>
+                                    </button>
+                                `
+                            )}
+                        </div>
+                    </div>
                 </div>
             `
         );
