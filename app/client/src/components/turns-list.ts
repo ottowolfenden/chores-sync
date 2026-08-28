36607694e3a90f938380abfe96574ff786a0a29a
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-28 17:20:27 +0100

    in turns list, show state on add button when dropdown button clicked

--- app/client/src/components/turns-list.ts
+++ app/client/src/components/turns-list.ts
@@ -4,6 +4,7 @@ import { repeat } from "lit/directives/repeat.js";
 import { addAssignment } from "../functions/db";
 import { Context } from "../classes/context";
 import { queryClosest } from "../functions/element-utils";
+import { ref } from "lit/directives/ref.js";
 
 @customElement("turns-list")
 export class TurnsList extends LitElement {
@@ -44,51 +45,60 @@ export class TurnsList extends LitElement {
         repeat(
             this.turns,
             t => t.chore.id,
-            t => html`
-                <div>
-                    <span class="chore-name">${t.chore.name}</span>
-                    <span class="member-name">${t.member.name}</span>
-                    <state-actions
-                        .conf=${{
-                            normal: {
-                                icon: "add",
-                                click: async () => await this.addAssignment(t)
-                            },
-                            loading: {},
-                            success: {},
-                            error: { msToShow: 2000 }
-                        }}
-                        state-button-class="transparent small">
-                    </state-actions>
-                    <button
-                        class="transparent small"
-                        popovertarget="turn-popover-${t.chore.id}${t.member.id}"
-                        style="anchor-name: --turn-anchor-${t.chore.id}${t.member.id}">
-                        <md-icon>arrow_drop_down</md-icon>
-                    </button>
-                    <div class="dropdown">
-                        <div
-                            popover
-                            class="dropdown"
-                            id="turn-popover-${t.chore.id}${t.member.id}"
-                            style="position-anchor: --turn-anchor-${t.chore.id}${t.member.id}">
-                            ${this.members.map(
-                                m => html`
-                                    <button
-                                        class="tonal"
-                                        @click=${async (e: Event) => {
-                                            queryClosest(e, "[popover]")?.hidePopover();
-                                            await this.addAssignment(t, m);
-                                            this.requestUpdate();
-                                        }}>
-                                        <md-icon>add</md-icon>
-                                        <span class="member-name">${m.name}</span>
-                                    </button>
-                                `
-                            )}
+            t => {
+                let stateActions: StateActions;
+                return html`
+                    <div>
+                        <span class="chore-name">${t.chore.name}</span>
+                        <span class="member-name">${t.member.name}</span>
+                        <state-actions
+                            .conf=${{
+                                normal: {
+                                    icon: "add",
+                                    click: async () => await this.addAssignment(t)
+                                },
+                                loading: {},
+                                success: {},
+                                error: { msToShow: 2000 }
+                            }}
+                            ${ref(el => (stateActions = el as StateActions))}
+                            state-button-class="transparent small">
+                        </state-actions>
+                        <button
+                            class="transparent small"
+                            popovertarget="turn-popover-${t.chore.id}${t.member.id}"
+                            style="anchor-name: --turn-anchor-${t.chore.id}${t.member.id}">
+                            <md-icon>arrow_drop_down</md-icon>
+                        </button>
+                        <div class="dropdown">
+                            <div
+                                popover
+                                id="turn-popover-${t.chore.id}${t.member.id}"
+                                style="position-anchor: --turn-anchor-${t.chore.id}${t.member
+                                    .id}">
+                                ${this.members.map(
+                                    m => html`
+                                        <button
+                                            class="tonal"
+                                            @click=${async (e: Event) => {
+                                                queryClosest(e, "[popover]")?.hidePopover();
+                                                if (stateActions) {
+                                                    stateActions.state = "loading";
+                                                    stateActions.handleResult(
+                                                        await this.addAssignment(t, m)
+                                                    );
+                                                }
+                                                this.requestUpdate();
+                                            }}>
+                                            <md-icon>add</md-icon>
+                                            <span class="member-name">${m.name}</span>
+                                        </button>
+                                    `
+                                )}
+                            </div>
                         </div>
                     </div>
-                </div>
-            `
+                `;
+            }
         );
 }
