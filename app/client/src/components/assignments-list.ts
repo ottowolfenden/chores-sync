8bae7b83e822df2c67a61756251b5011ae82a53a
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-01 19:56:09 +0100

    move ts out of html and refactor

--- app/client/src/components/assignments-list.ts
+++ app/client/src/components/assignments-list.ts
@@ -13,15 +13,8 @@ export class AssignmentsList extends LitElement {
     @property({ type: Array }) assignments: UiAssignment[] = [];
 
     @property({ type: Boolean, attribute: "edit-mode" }) editMode = false;
-    updated = () => this.toggleAttribute("inert", !this.editMode);
 
     @state() private members: UiMember[] = [];
-
-    private addAssignment = (e: Event) =>
-        (this.assignments = cloneAndSum([
-            ...this.assignments,
-            (e as CustomEvent).detail.assignment
-        ]));
 
     async connectedCallback() {
         super.connectedCallback();
@@ -37,63 +30,70 @@ export class AssignmentsList extends LitElement {
     protected update(changed: PropertyValues) {
         super.update(changed);
         addHaptics("button");
+        this.toggleAttribute("inert", !this.editMode);
     }
 
-    private removeAssignment = async (e: Event, assignment: UiAssignment) =>
+    private readonly addAssignment = (e: Event) =>
+        (this.assignments = cloneAndSum([
+            ...this.assignments,
+            (e as CustomEvent).detail.assignment
+        ]));
+
+    private readonly removeAssignment = async (e: Event, assignment: UiAssignment) =>
         await withTransition(queryClosest(e, ".assignment"), {
             before: { "height": "0", "opacity": "0", "margin-top": "0" },
             after: () =>
                 (this.assignments = this.assignments.filter(a => a.uuid != assignment.uuid))
         });
 
-    private readonly getAssignmentHTML = (a: UiAssignment) => html`
-        <div class="assignment">
-            <span class="chore-name">${a.chore.name}</span>
-            <span class="quantity" ?hidden=${a.quantity < 2}>×${a.quantity}</span>
-            <md-icon class="swapped" ?hidden=${a.chosenMember.id == a.turnMember.id}>
-                swap_horiz
-            </md-icon>
-            <div class="dropdown">
-                <button
-                    class="tonal"
-                    popovertarget="assignment-popover-${a.uuid}"
-                    style="anchor-name: --assignment-anchor-${a.uuid}">
-                    <span class="member-name">${a.chosenMember.name}</span>
-                    <md-icon>arrow_drop_down</md-icon>
-                </button>
-                <div
-                    popover
-                    id="assignment-popover-${a.uuid}"
-                    style="position-anchor: --assignment-anchor-${a.uuid}">
-                    ${this.members
-                        .filter(m => m.id != a.chosenMember.id)
-                        .map(
-                            m => html`
-                                <button
-                                    class="tonal"
-                                    @click=${(e: Event) => {
-                                        queryClosest(e, "[popover]")?.hidePopover();
-                                        a.chosenMember = m;
-                                        this.requestUpdate();
-                                    }}>
-                                    <span class="member-name">${m.name}</span>
-                                </button>
-                            `
-                        )}
-                </div>
-            </div>
-            <button
-                class="remove tonal small"
-                @click=${async (e: Event) => await this.removeAssignment(e, a)}>
-                <md-icon>remove</md-icon>
-            </button>
-        </div>
-    `;
+    private readonly changeMember = (e: Event, assignment: UiAssignment, member: UiMember) => {
+        queryClosest(e, "[popover]")?.hidePopover();
+        assignment.chosenMember = member;
+        this.requestUpdate();
+    };
 
     render = () =>
         repeat(
             this.assignments,
             a => a.uuid,
-            a => this.getAssignmentHTML(a)
+            a => html`
+                <div class="assignment">
+                    <span class="chore-name">${a.chore.name}</span>
+                    <span class="quantity" ?hidden=${a.quantity < 2}>×${a.quantity}</span>
+                    <md-icon class="swapped" ?hidden=${a.chosenMember.id == a.turnMember.id}>
+                        swap_horiz
+                    </md-icon>
+                    <div class="dropdown">
+                        <button
+                            class="tonal"
+                            popovertarget="today-assignment-popover-${a.uuid}"
+                            style="anchor-name: --today-assignment-anchor-${a.uuid}">
+                            <span class="member-name">${a.chosenMember.name}</span>
+                            <md-icon>arrow_drop_down</md-icon>
+                        </button>
+                        <div
+                            popover
+                            id="today-assignment-popover-${a.uuid}"
+                            style="position-anchor: --today-assignment-anchor-${a.uuid}">
+                            ${this.members
+                                .filter(m => m.id != a.chosenMember.id)
+                                .map(
+                                    m => html`
+                                        <button
+                                            class="tonal"
+                                            @click=${(e: Event) => this.changeMember(e, a, m)}>
+                                            <span class="member-name">${m.name}</span>
+                                        </button>
+                                    `
+                                )}
+                        </div>
+                    </div>
+                    <button
+                        class="remove tonal small"
+                        @click=${async (e: Event) => await this.removeAssignment(e, a)}>
+                        <md-icon>remove</md-icon>
+                    </button>
+                </div>
+            `
         );
 }
