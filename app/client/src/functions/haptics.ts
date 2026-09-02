f99d786bce689f3e6465c4c3b4a9d17e7d14bc90
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-02 01:19:28 +0100

    prevent exceptions in firefox when navigator.vibrate is called on desktop

--- app/client/src/functions/haptics.ts
+++ app/client/src/functions/haptics.ts
@@ -3,12 +3,17 @@ export const addHaptics = (
     rootNode?: ParentNode,
     ms?: number
 ) => {
+    if (!("vibrate" in navigator)) return;
+
     const query = (sel: string) => [...(rootNode ?? document).querySelectorAll(sel)];
     let elsToAdd: Element[] = [];
-
     if (typeof els == "string") elsToAdd = query(els);
     else if (els instanceof Element) elsToAdd = [els];
     else els.forEach(el => elsToAdd.concat(typeof el == "string" ? query(el) : [el]));
 
-    elsToAdd.forEach(el => el.addEventListener("click", () => navigator.vibrate(ms ?? 1)));
+    const vibrate = () => navigator.vibrate(ms ?? 1);
+    elsToAdd.forEach(el => {
+        el.removeEventListener("click", vibrate);
+        el.addEventListener("click", vibrate);
+    });
 };
