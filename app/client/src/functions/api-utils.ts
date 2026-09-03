e093ea2ef70f888685c5489b71b756d5ae1d4830
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-03 02:20:02 +0100

    complete request method by adding other http methods and defining the type of data passed in for each

--- app/client/src/functions/api-utils.ts
+++ app/client/src/functions/api-utils.ts
@@ -1,7 +1,18 @@
-export const request = async <T = unknown>(
-    method: "GET" | "PUT",
+type Method = "GET" | "DELETE" | "HEAD" | "PUT" | "POST" | "PATCH";
+
+type Data<M extends Method> = {
+    "GET": undefined;
+    "DELETE": undefined;
+    "HEAD": Record<string, string>;
+    "PUT": unknown;
+    "POST": unknown;
+    "PATCH": unknown;
+}[M];
+
+export const request = async <T = unknown, M extends Method = Method>(
+    method: M,
     endpoint: string,
-    body?: unknown
+    data?: Data<M>
 ): Promise<{ ok: boolean; data?: T | null }> => {
     const timeout = 10000;
     const guess = localStorage.getItem("secret");
@@ -10,18 +21,27 @@ export const request = async <T = unknown>(
     let response: Response | null;
     const init: RequestInit = {
         method,
-        headers: { "Authorization": guess },
+        headers: { "Authorization": guess, "Content-Type": "application/json" },
         signal: AbortSignal.timeout(timeout)
     };
 
     switch (method) {
         case "GET":
+        case "DELETE":
             response = await fetch(endpoint, init).catch(() => null);
             return { ok: response?.ok ?? false, data: await response?.json() };
-        case "PUT":
+        case "HEAD":
             response = await fetch(endpoint, {
                 ...init,
-                body: JSON.stringify(body)
+                headers: { ...init.headers, ...(data as Data<"HEAD">) }
+            }).catch(() => null);
+            return { ok: response?.ok ?? false };
+        case "PUT":
+        case "POST":
+        case "PATCH":
+            response = await fetch(endpoint, {
+                ...init,
+                body: JSON.stringify(data)
             }).catch(() => null);
             return { ok: response?.ok ?? false };
         default:
