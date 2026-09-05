6dffc71c03c4e16eff8bc1c411d96378f4fcb0e5
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-06 00:42:29 +0100

    add event listener to recentre button

--- app/client/src/sections/timeline.ts
+++ app/client/src/sections/timeline.ts
@@ -3,6 +3,20 @@ import "../components/timeline-list.js";
 const section = document.querySelector("section#timeline")!;
 
 section.addEventListener("open", () => {
-    section.addEventListener("close", () => {}, { once: true });
-    console.log("timeline");
+    const ui = {
+        timelineList: section.querySelector("timeline-list")!,
+        recentreButton: section.querySelector("#recentre")!,
+        selectDateButton: section.querySelector("#select-date")!
+    };
+
+    const recentre = () => ui.timelineList.scrollToDate();
+    ui.recentreButton.addEventListener("click", recentre);
+
+    section.addEventListener(
+        "close",
+        () => {
+            ui.recentreButton.removeEventListener("click", recentre);
+        },
+        { once: true }
+    );
 });
