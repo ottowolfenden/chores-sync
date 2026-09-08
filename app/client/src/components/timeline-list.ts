82c2e9d7053aff1557cc5d6f94229fea332343ce
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-08 22:38:19 +0100

    fix opacity animation being cancelled by instantly by only collapsing all when necessary

--- app/client/src/components/timeline-list.ts
+++ app/client/src/components/timeline-list.ts
@@ -130,7 +130,11 @@ export class TimelineList extends LitElement {
     toggleExpand = ({ date, e }: { date?: Date | string; e?: Event }) => {
         const dateEl = e ? queryClosest(e, "[data-date]") : this.getDateEl(date);
         const expanded = dateEl?.toggleAttribute("data-expanded");
-        if (expanded) this.collapseAll({ exclude: dateEl, instant: true });
+        if (
+            expanded &&
+            this.container.querySelectorAll("[data-date][data-expanded]").length > 1
+        )
+            this.collapseAll({ exclude: dateEl, instant: true });
         dateEl
             ?.querySelector<MdIcon>(".expand md-icon")
             ?.setIcon(expanded ? "keyboard_arrow_up" : "keyboard_arrow_down");
