a1b4a436845c3e1cb21afdf2c2f98e6e4212d608
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-12 00:57:05 +0100

    move material symbols definition file into client folder and rewrite fetch script

--- app/client/src/components/easter-egg-game.ts
+++ app/client/src/components/easter-egg-game.ts
@@ -1,9 +1,14 @@
 import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators.js";
+import data from "../assets/material-symbols.json";
 
 @customElement("easter-egg-game")
 export class EasterEggGame extends LitElement {
     protected createRenderRoot = () => this;
+
+    connectedCallback(): void {
+        console.log(data);
+    }
 
     render = () => html`<md-icon>bathtub</md-icon>`;
 }
--- scripts/fetch-material-symbols.mjs
+++ scripts/fetch-material-symbols.mjs
@@ -1,32 +1,24 @@
 import fs from "node:fs";
 
-const outDir = "app/client/src/assets/fonts/";
+const api = "https://fonts.googleapis.com/css2";
 const userAgent = {
     "User-Agent":
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36"
 };
 
-const download = async icons => {
-    const cssUrl =
-        "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:" +
-        `opsz,wght,FILL@20..48,100..700,0..1&icon_names=${icons}`;
-    const css = await fetch(cssUrl, { headers: userAgent }).then(r => r.text());
-    const start = css.indexOf("url(") + 4;
-    const end = css.indexOf(")", start);
-    const fontUrl = css.substring(start, end);
-    return fetch(fontUrl).then(r => r.arrayBuffer());
-};
-
-const write = async (name, font) => fs.writeFileSync(outDir + name, Buffer.from(await font));
-
-try {
-    const [main, easterEgg] = fs
-        .readFileSync("material-symbols.txt", "utf-8")
-        .split("EASTER EGG ICONS")
-        .map(l => download([...new Set(l.trim().split(/\s+/))].sort()));
-    await write("material-symbols.woff2", main);
-    await write("material-symbols-easter-egg.woff2", easterEgg);
-    console.log("successfully downloaded material symbols");
-} catch (err) {
-    console.error(`failed to download material symbols\n${err}`);
-}
+await Promise.all(
+    JSON.parse(fs.readFileSync("app/client/src/assets/material-symbols.json")).map(
+        async obj => {
+            const icons = [...new Set(obj.icons)].join(",");
+            const cssUrl = `${api}?family=Material+Symbols+Outlined:${obj.config}&icon_names=${icons}`;
+            const css = await fetch(cssUrl, { headers: userAgent }).then(r => r.text());
+            const start = css.indexOf("url(") + 4;
+            const end = css.indexOf(")", start);
+            const fontUrl = css.substring(start, end);
+            const font = await fetch(fontUrl).then(r => r.arrayBuffer());
+            fs.writeFileSync(obj.path, Buffer.from(font));
+        }
+    )
+)
+    .then(() => console.log("successfully downloaded material symbols"))
+    .catch(err => console.error(`failed to download material symbols\n${err}`));
