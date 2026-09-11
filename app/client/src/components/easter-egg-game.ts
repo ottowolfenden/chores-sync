893d34af9f72dddd07c367bc46328ccc3a289b38
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-11 02:54:26 +0100

    begin writing step system

--- app/client/src/components/easter-egg-game.ts
+++ app/client/src/components/easter-egg-game.ts
@@ -1,6 +1,8 @@
 import { LitElement, html } from "lit";
 import { customElement, query } from "lit/decorators.js";
-import { throttle } from "../functions/timer";
+import { delay, throttle } from "../functions/timer";
+
+export type Step = { delay: number; allowed: ["top"] | ["bottom"] | ["top", "bottom"] };
 
 @customElement("easter-egg-game")
 export class EasterEggGame extends LitElement {
@@ -8,8 +10,13 @@ export class EasterEggGame extends LitElement {
 
     @query(".player") private player!: MdIcon;
     private active = false;
-    private started = false;
+    private running = false;
+    private runId = 0;
     private pos: "top" | "bottom" = "bottom";
+    private steps: Step[] = [
+        { delay: 1000, allowed: ["top", "bottom"] },
+        { delay: 4000, allowed: ["top"] }
+    ];
 
     disconnectedCallback() {
         super.disconnectedCallback();
@@ -28,32 +35,44 @@ export class EasterEggGame extends LitElement {
         this.active = false;
     };
 
-    private start = () => {
-        if (!this.active) return;
-        this.started = true;
+    private start = async () => {
+        if (!this.active || this.running) return;
+        this.running = true;
+        const id = ++this.runId;
+        console.log("started");
+
+        for (const step of this.steps) {
+            await delay(step.delay);
+            if (id != this.runId) return;
+            console.log(step.allowed);
+        }
+        this.stop();
     };
 
     private stop = () => {
-        this.started = false;
+        this.running = false;
+        this.runId++;
+        console.log("stopped");
     };
 
     private handleKeydown = (e: KeyboardEvent) => {
-        if (["Enter", " "].includes(e.key)) (this.started ? this.handlePress : this.start)();
+        if (["Enter", " "].includes(e.key)) (this.running ? this.handlePress : this.start)();
+        else if (e.key == "s") this.stop();
     };
 
     private handlePress = throttle(() => {
-        this.player.style.animationName = this.pos == "top" ? "flip-down" : "flip-up";
         this.pos = this.pos == "top" ? "bottom" : "top";
+        this.player.style.animationName = `jump-to-${this.pos}`;
     }, 400);
 
     render = () => html`
         <div class="stats"></div>
         <div
             class="container"
-            @mousedown=${() => (this.started ? this.handlePress : this.start)()}>
+            @mousedown=${() => (this.running ? this.handlePress : this.start)()}>
             <md-icon class="player">directions_bike</md-icon>
-            <div class="platform"></div>
-            <div class="platform" data-top></div>
+            <!-- <div class="platform"></div>
+            <div class="platform" data-top></div> -->
         </div>
     `;
 }
