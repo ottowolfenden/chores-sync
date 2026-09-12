7435e0931b0dd10f5affab07da6078c68455d3e5
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-12 02:10:55 +0100

    write main logic for matching game

--- app/client/src/components/easter-egg-game.ts
+++ app/client/src/components/easter-egg-game.ts
@@ -1,14 +1,39 @@
 import { LitElement, html } from "lit";
-import { customElement } from "lit/decorators.js";
-import data from "../assets/material-symbols.json";
+import { customElement, state } from "lit/decorators.js";
+import { getRandFrom, getRandsFrom, shuffle } from "../functions/rand-utils";
+import materialSymbols from "../assets/material-symbols.json";
 
 @customElement("easter-egg-game")
 export class EasterEggGame extends LitElement {
     protected createRenderRoot = () => this;
 
-    connectedCallback(): void {
-        console.log(data);
+    private readonly numPerCard = 8;
+
+    @state() private cards?: string[][];
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.generateCards();
     }
 
-    render = () => html`<md-icon>bathtub</md-icon>`;
+    private generateCards = () => {
+        let symbols = [
+            ...new Set(materialSymbols.find(obj => obj.for == "easter-egg")?.icons)
+        ];
+        if (!symbols) return;
+        const match = getRandFrom(symbols);
+        symbols = symbols.filter(s => s != match);
+        const symbols1 = getRandsFrom(symbols, this.numPerCard - 1);
+        symbols = symbols.filter(s => !symbols1.some(s1 => s1 == s));
+        const symbols2 = getRandsFrom(symbols, this.numPerCard - 1);
+        this.cards = [
+            [match, ...symbols1],
+            [match, ...symbols2]
+        ].map(shuffle);
+    };
+
+    render = () =>
+        this.cards?.map(
+            c => html`<div class="card">${c.map(i => html`<md-icon>${i}</md-icon>`)}</div>`
+        );
 }
