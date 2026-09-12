8713f46f7207bfc9ae43e322a8ba7fcd5fef6c68
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-12 02:08:27 +0100

    add functions for selecting random numbers and shuffling

---
+++ app/client/src/functions/rand-utils.ts
@@ -0,0 +1,26 @@
+export const getRandInt = (min: number, max: number) =>
+    Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min));
+
+export const getRandFrom = <T>(array: T[]) => array[getRandInt(0, array.length - 1)]!;
+
+export const getRandsFrom = <T>(array: T[], n: number) => {
+    let clone = structuredClone(array);
+    let result = [];
+    for (let i = 0; i < n; i++) {
+        const item = clone[getRandInt(0, clone.length - 1)]!;
+        clone.splice(clone.indexOf(item), 1);
+        result.push(item);
+    }
+    return result;
+};
+
+export const shuffle = <T>(array: T[]) => {
+    let clone = structuredClone(array);
+    let i = clone.length;
+    while (i > 0) {
+        let randIndex = getRandInt(0, clone.length - 1);
+        i--;
+        [clone[i], clone[randIndex]] = [clone[randIndex]!, clone[i]!];
+    }
+    return clone;
+};
