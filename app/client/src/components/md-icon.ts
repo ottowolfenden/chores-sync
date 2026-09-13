fd4ce79ab3a68977d6945478607b4e9926be3d22
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-13 23:50:28 +0100

    revert md icon component

--- app/client/src/components/md-icon.ts
+++ app/client/src/components/md-icon.ts
@@ -1,13 +1,9 @@
 import { LitElement, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement } from "lit/decorators.js";
 
 @customElement("md-icon")
 export class MdIcon extends LitElement {
     protected createRenderRoot = () => this;
-
-    @property({ type: Boolean }) fill = false;
-
-    updated = () => this.style.setProperty("--fill", (this.fill ? 1 : 0).toString());
     setIcon = (icon: string) => (this.textContent = icon);
     render = () => html`<slot></slot>`;
 }
