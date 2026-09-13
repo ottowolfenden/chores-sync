0121887666757d75dc9f2b616a1528fb8a97ba32
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-13 16:58:21 +0100

    add timer progress bar and state-actions to start and reset

--- app/client/src/components/easter-egg-game.ts
+++ app/client/src/components/easter-egg-game.ts
@@ -1,7 +1,8 @@
 import { LitElement, html } from "lit";
-import { customElement, state } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 import { getRandFrom, getRandInt, getRandsFrom, shuffle } from "../functions/rand-utils";
 import materialSymbols from "../assets/material-symbols.json";
+import type { Conf } from "./state-actions";
 
 export type Symbol = { icon: string; rotation: number };
 export type Card = { symbols: Symbol[]; variation: number; rotation: number };
@@ -11,12 +12,38 @@ export class EasterEggGame extends LitElement {
     protected createRenderRoot = () => this;
 
     private readonly numPerCard = 8;
-    @state() private cards?: Card[];
+    private readonly gameDuration = 40_000;
+    private readonly emptyCards: Card[] = [
+        { symbols: [], variation: 0, rotation: 0 },
+        { symbols: [], variation: 0, rotation: 0 }
+    ];
+    private timer?: number;
 
-    connectedCallback() {
-        super.connectedCallback();
+    @property({ type: Boolean }) running: boolean = false;
+    @state() timeRemaining = this.gameDuration;
+    @state() private cards: Card[] = this.emptyCards;
+
+    private start = () => {
+        if (this.running) return;
         this.generateCards();
-    }
+        this.running = true;
+        const endTime = Date.now() + this.gameDuration;
+        this.timer = setInterval(() => {
+            this.timeRemaining = Math.max(endTime - Date.now(), 0);
+            if (this.timeRemaining == 0) this.resetTimer();
+        }, 200);
+    };
+
+    private reset = () => {
+        this.running = false;
+        this.resetTimer();
+        this.cards = this.emptyCards;
+    };
+
+    private resetTimer = () => {
+        clearInterval(this.timer);
+        this.timeRemaining = this.gameDuration;
+    };
 
     private generateCards = () => {
         let symbols = [
@@ -37,22 +64,46 @@ export class EasterEggGame extends LitElement {
                 icon,
                 rotation: getRandInt(0, 360)
             })),
-            variation: getRandInt(1, 10),
+            variation: 1,
             rotation: getRandInt(0, 360)
         }));
     };
 
-    render = () =>
-        this.cards?.map(
-            c => html`
-                <div
-                    class="card"
-                    data-variation=${c.variation}
-                    style="rotate:${c.rotation}deg">
-                    ${c.symbols.map(
-                        s => html`<md-icon style="rotate:${s.rotation}deg">${s.icon}</md-icon>`
-                    )}
-                </div>
-            `
-        );
+    render = () => html`
+        <div class="cards-container">
+            ${this.cards.map(
+                c => html`
+                    <div class="card" data-variation=${c.variation}>
+                        ${c.symbols.map(
+                            s => html`
+                                <button class="transparent">
+                                    <md-icon style="rotate:${s.rotation}deg">
+                                        ${s.icon}
+                                    </md-icon>
+                                </button>
+                            `
+                        )}
+                    </div>
+                `
+            )}
+        </div>
+        <progress value=${this.timeRemaining} max=${this.gameDuration}></progress>
+        <state-actions
+            .conf=${{
+                normal: {
+                    icon: "play_arrow",
+                    label: "Start",
+                    class: "filled",
+                    click: this.start
+                },
+                active: {
+                    icon: "restart_alt",
+                    label: "Reset",
+                    class: "tonal",
+                    withTransition: false,
+                    click: this.reset
+                }
+            } as Conf}>
+        </state-actions>
+    `;
 }
