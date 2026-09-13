7612c4b4292faf67af36e8238baf18935bef1bca
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-13 23:43:27 +0100

    add ability to fill icon via a property

--- app/client/src/components/md-icon.ts
+++ app/client/src/components/md-icon.ts
@@ -1,9 +1,13 @@
 import { LitElement, html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement("md-icon")
 export class MdIcon extends LitElement {
     protected createRenderRoot = () => this;
+
+    @property({ type: Boolean }) fill = false;
+
+    updated = () => this.style.setProperty("--fill", (this.fill ? 1 : 0).toString());
     setIcon = (icon: string) => (this.textContent = icon);
     render = () => html`<slot></slot>`;
 }
