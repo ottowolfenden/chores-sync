68f9938ac82ff803900cb8413ce612f4fb92e364
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-14 19:47:59 +0100

    increase boost

--- app/client/src/components/easter-egg-game.ts
+++ app/client/src/components/easter-egg-game.ts
@@ -17,7 +17,7 @@ export class EasterEggGame extends LitElement {
     private readonly duration = 60_000;
     private readonly maxLives = 3;
     private readonly penalty = 2000;
-    private readonly boost = 2000;
+    private readonly boost = 3000;
     private timer?: number;
     private endTime: number = Infinity;
 
