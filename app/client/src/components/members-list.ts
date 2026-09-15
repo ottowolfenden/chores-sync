22a928d88b510a939d164f76759f2ee0a8ad939b
Otto Wolfenden <ottowolfenden@gmail.com>
2026-09-15 22:21:27 +0100

    write ui for members list

--- app/client/src/components/members-list.ts
+++ app/client/src/components/members-list.ts
@@ -1,10 +1,31 @@
 import { LitElement, html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
+import { repeat } from "lit/directives/repeat.js";
 import "../components/toggle-switch";
 
 @customElement("members-list")
 export class MembersList extends LitElement {
     protected createRenderRoot = () => this;
 
-    render = () => html`members list`;
+    @property({ type: Array }) members: UiMember[] = [];
+    @property({ type: Object }) currentMember?: UiMember;
+
+    render = () =>
+        repeat(
+            this.members,
+            m => m.id,
+            m => html`
+                <div class="member">
+                    <span class="name">${m.name}</span>
+                    <toggle-switch
+                        text="Admin"
+                        .on=${m.isAdmin}
+                        ?disabled=${!this.currentMember?.isAdmin}></toggle-switch>
+                    <toggle-switch
+                        text="Active"
+                        .on=${m.isActive}
+                        ?disabled=${!this.currentMember?.isAdmin}></toggle-switch>
+                </div>
+            `
+        );
 }
--- app/client/src/sections/settings.ts
+++ app/client/src/sections/settings.ts
@@ -1,6 +1,22 @@
+import { Cache } from "../classes/cache";
 import "../components/members-list";
 
 const section = document.querySelector("section#settings")!;
+const membersList = section.querySelector("members-list")!;
+const message = section.querySelector("status-message")!;
 
-section.addEventListener("sectionopen", () => console.log("settings opened"));
+message.elsToHide = [membersList];
+
+section.addEventListener("sectionopen", async () => {
+    message.status = "loading";
+    const members = await Cache.members.get();
+    const currentMember = await Cache.currentMember.get();
+    if (!members || !currentMember) message.status = "error";
+    else if (members.length == 0) message.status = "empty";
+    else {
+        message.status = "success";
+        membersList.members = members;
+        membersList.currentMember = currentMember;
+    }
+});
 section.addEventListener("sectionclose", () => console.log("settings closed"));
